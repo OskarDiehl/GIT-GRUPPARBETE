9f8c554d71b1dc7867f81149806c8be740060c1e
LucasEdwa <lucas.eduardo2070@gmail.com>
2024-12-13 10:33:12 +0100

    hero-main settup

--- git_grupparbete/src/main.ts
+++ git_grupparbete/src/main.ts
@@ -1,4 +1,8 @@
+import { createHeroMain } from "./components/hero-main/heroMain";
 import { createNavbar } from "./components/navBar/navBar";
 
 const app: HTMLElement | null = document.getElementById('app');
+
+
 createNavbar(app);
+createHeroMain(app);
